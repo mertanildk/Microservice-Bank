1b671126a7ffeca17cffb45a5d55a9e4904d8669
MertAnil <107416952+mertanildk@users.noreply.github.com>
2023-06-14 14:57:44 +0300

    Authorization Server && Web Security Configuration aare completed. when We post "/auth/token" endpoint, it gives token and refresh token too.

--- user.oauth2.0/src/main/java/com/springbank/user/oauth20/services/UserService.java
+++ user.oauth2.0/src/main/java/com/springbank/user/oauth20/services/UserService.java
@@ -1,32 +1,40 @@
 package com.springbank.user.oauth20.services;
 
 import com.springbank.user.oauth20.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 
-public class UserServiceImpl implements UserDetailsService {
+@Service(value = "userService")
+public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    @Autowired
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        var user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        var user = userRepository.findByUsername(username);
+
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("Incorrect Username / Password supplied!");
+        }
+
+        var account = user.get().getAccount();
 
         return org.springframework.security.core.userdetails.User
                 .withUsername(username)
-                .password(user.getAccount().getPassword())
+                .password(account.getPassword())
+                .authorities(account.getRoles())
                 .accountExpired(false)
                 .accountLocked(false)
                 .credentialsExpired(false)
                 .disabled(false)
                 .build();
-
     }
 }
