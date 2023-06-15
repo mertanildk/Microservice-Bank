955fd3bf906bd1ee8788bf8e0387e9dc311d70da
MertAnil <107416952+mertanildk@users.noreply.github.com>
2023-06-15 10:18:27 +0300

    api gateway added

--- user.core/src/main/java/com/springbank/user/core/messages/ResponseMessages.java
+++
@@ -1,7 +0,0 @@
-package com.springbank.user.core.messages;
-
-public class ResponseMessages {
-    public static String USER_FAILED_BY_ID = "Failed to complete get user by id request";
-    public static String USERS_FAILED_FOR_ALL = "Failed to complete get all users request";
-    public static String USERS_FAILED_BY_FILTERS = "Failed to complete get users by filters request";
-}
--- user.query.api/src/main/java/com/springbank/user/query/api/queries/FindAllUsersQuery.java
+++
@@ -1,7 +0,0 @@
-package com.springbank.user.query.api.queries;
-
-import lombok.Data;
-
-@Data
-public class FindAllUsersQuery {
-}
